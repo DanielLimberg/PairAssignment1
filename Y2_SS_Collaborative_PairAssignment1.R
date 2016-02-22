66a1de4dac762cb18345f1cc171f0fc523c55ef0
DanielLimberg <d.limberg@alumni.maastrichtuniversity.nl>
2016-02-22 17:27:27 +0100

    dataset
    dataset ToothGrowth

---
+++ Y2_SS_Collaborative_PairAssignment1.R
@@ -0,0 +1,18 @@
+##CSSD Pair Assignment 1
+
+#load packages
+library(ggplot2)
+library(dplyr)
+library(magrittr)
+library(knitr)
+
+#working directory
+try("C:/Users/Dani/Documents/GitHub2/PairAssignment1")
+try()
+setwd()
+
+#load dataset
+data("ToothGrowth")
+
+#information on dataset
+?ToothGrowth
