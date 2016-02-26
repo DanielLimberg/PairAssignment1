76014004b5e0c8cd2d1beecaa723043d7d5fe334
LukasMueller89 <l.mueller@mpp.hertie-school.org>
2016-02-26 12:03:14 +0100

    Update Histograms

--- Descriptive Statistics.R
+++ Descriptive Statistics.R
@@ -15,59 +15,90 @@ teeth <- ToothGrowth[,c(2,1,3)]
 
 # Mesures of Central Tendency: Mean | Median | Histogram
 
-## Loop for Mean of each Variable (NA for supply(2) which is nominal)
-for (i in 1:3) {
-  ToothGrowth[, i] %>%
+## Loop for Mean of each Variable except supply (nominal)
+for (i in 2:3) {
+  teeth[, i] %>%
     mean() %>%
     round(digits = 2) %>%
-    paste(names(ToothGrowth)[i], ., "\n") %>%
+    paste(names(teeth)[i], ., "\n") %>%
     cat()
 }
  
-## Loop for Median of each Variable (supply (2) is nominal)
-median(ToothGrowth$len)
-median(ToothGrowth$dose)
+## Loop for Median of each Variable except supply (nominal)
+for (i in 2:3) {
+  teeth[, i] %>%
+    median() %>%
+    round(digits = 2) %>%
+    paste(names(teeth)[i], ., "\n") %>%
+    cat()
+}
+
+## Distribution of Variables
+
+### Length of Teeth
+hist(teeth$len,
+     main="Length of Teeth", 
+     col="blue", 
+     breaks = 20,
+     xlab = "Length in mm",
+     ylab = "Number of Guinea Pigs")
+
+### Dose in mg/days
+hist(teeth$dose,
+     main="Dose", 
+     col="red", 
+     breaks = 10,
+     xlab = "Dose in mg/days",
+     ylab = "Number of Guinea Pigs")
+
+### Frequency of Supplement
+plot(teeth$supp, xlab = "Supplement")
+
+?ToothGrowth 
+
+
+
 
 # Measures of Dispersion: Range | IQR | Standard Deviation | Boxplots | Variance
 
 ## Range
-range(ToothGrowth$dose)
+range(teeth$dose)
 for (i in 1:2) {
-  ToothGrowth[, i] %>%
+  teeth[, i] %>%
     range() %>%
-    paste(names(ToothGrowth)[i], ., "\n") %>%
+    paste(names(teeth)[i], ., "\n") %>%
     cat()
 }
 
 ## Quartiles
-summary(ToothGrowth$len)
+summary(teeth$len)
 for (i in 1:3) {
-  ToothGrowth[, i] %>%
+  teeth[, i] %>%
     summary() %>%
-    paste(names(ToothGrowth)[i], ., "\n") %>%
+    paste(names(teeth)[i], ., "\n") %>%
     cat()
 }
 
 ## Interquartile Range
-IQR(ToothGrowth$len)
+IQR(teeth$len)
 for (i in 1:3) {
-  ToothGrowth[, i] %>%
+  teeth[, i] %>%
     IQR() %>%
-    paste(names(ToothGrowth)[i], ., "\n") %>%
+    paste(names(teeth)[i], ., "\n") %>%
     cat()
 }
 
 ## Boxplots
-boxplot(ToothGrowth$len, main = 'Length of Teeth')
+boxplot(teeth$len, main = 'Length of Teeth')
 for (i in 1:3) {
-  ToothGrowth[, i] %>%
+  teeth[, i] %>%
     boxplot() %>%
-    paste(names(ToothGrowth)[i], ., "\n") %>%
+    paste(names(teeth)[i], ., "\n") %>%
     cat()
 }
 
 ## Variance: two ways to calculate
-x <- ToothGrowth$len
+x <- teeth$len
 sum( (x - mean(x) )^2 )
 var(x) * (length(x) - 1)
 rm(i, x)
