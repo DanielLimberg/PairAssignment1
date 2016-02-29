f7a5ae0b4ecc4f275c3f2cbf0cc8bcf14a3da556
DanielLimberg <d.limberg@alumni.maastrichtuniversity.nl>
2016-02-29 19:14:24 +0100

    ## Commented
    commented, and deleted redundant parts

--- PairAssignment1_JointDistributions_ToothGrowth.R
+++ PairAssignment1_JointDistributions_ToothGrowth.R
@@ -20,23 +20,30 @@ qplot(dose, len, data=teeth)
 qplot(supp, len, data=teeth)
 
 # Test of statistical significance
-
-## Difference in means test
+# Difference in means test
 teeth$supp <- as.factor(teeth$supp)
 temp <- teeth$supp == "OJ"
-OJ <- teeth[temp,]$len
+OJ <- teeth[temp,]$len #supplement 'OJ'
 VC <- teeth[!temp,]$len #supplement 'VC'
+
+## The 60 pigs become separeted in two groups according to the supplement they were given.
+## Both groups are put into an object.
 
 t.test(OJ, VC)
 
+## A difference in means test is conducted to see whether the difference between the two groups is stat. sign.
+## The propability that the Null is true is 6.1%. That means that the Null cannot be rejected.
+
 rm(temp, OJ, VC)
 
-## Chi square test
+# Chi square test
 teeth$len2 <- cut(teeth$len, c(0,12,19,25.2,40))
 teeth$len2 <- as.factor(teeth$len2)
-
-tbl <- table(teeth$supp, teeth$len2)
-chisq.test(tbl)
+## Tooth lenght is divided into four groups.
 
 tbl <- table(teeth$dose, teeth$len2)
 chisq.test(tbl)
+## Chi squ test to see whether there is a statistical sign. relationship between the dose and the length.
+## The result p<0.05 indicates that there is a sign. relationship between the dose and the group of length.
+
+rm(tbl)
